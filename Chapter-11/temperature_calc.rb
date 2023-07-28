8a64408e6e2e375c09875554e10bf3ff7cfd6cee
Andreas Bourakis <bourakis@gmail.com>
2023-07-28 16:22:49 +0300

    Create temperature_calc.rb

---
+++ Chapter-11/temperature_calc.rb
@@ -0,0 +1,10 @@
+# Average temperature calculation
+# without using arrays.
+temp1 = 25
+temp2 = 26
+temp3 = 28
+temp4 = 30
+temp5 = 33
+sum = temp1 + temp2 + temp3 + temp4 + temp5
+average = sum / 5
+puts “Average temperature: ” + average.to_s
