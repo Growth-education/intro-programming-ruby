b9b487f01aaae69747b288a51992402b114a8ffa
Andreas Bourakis <bourakis@gmail.com>
2023-07-28 12:07:57 +0300

    Create example6-7.rb

---
+++ Chapter-6/example6-7.rb
@@ -0,0 +1,12 @@
+x = 5
+if x == 3
+  puts "x is 3"
+elsif x == 4
+  puts "x is 4"
+elsif x == 5
+  puts "x is 5"
+elsif x == 6
+ puts "x is 6"
+elsif x == 7
+  puts "x is 7"                          
+end
