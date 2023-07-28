578b3f1d3718eedec4588c1df8d85b34fb7aa774
Andreas Bourakis <bourakis@gmail.com>
2023-07-28 15:50:08 +0300

    Create mult_table2.rb

---
+++ Chapter-10/mult_table2.rb
@@ -0,0 +1,17 @@
+ puts "1 times table"
+ for i in 1..10 do
+    result = 1 * i
+    puts "1 x " + i.to_s + " = " + result.to_s
+ end
+   
+ puts "2 times table"
+ for i in 1..10 do
+    result = 2 * i
+    puts "2 x " + i.to_s + " = " + result.to_s        
+ end
+ 
+ puts "3 times table"
+ for i in 1..10 do
+    result = 3 * i
+    puts "3 x " + i.to_s + " = " + result.to_s
+ end
