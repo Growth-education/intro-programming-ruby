771a95c264e4228ee390b165ee819903de461550
Andreas Bourakis <bourakis@gmail.com>
2023-07-28 12:10:46 +0300

    Create example6-8.rb

---
+++ Chapter-6/example6-8.rb
@@ -0,0 +1,16 @@
+x = 5
+if x == 3
+  puts "x is 3"
+end
+if x == 4
+  puts "x is 4"
+end
+if x == 5
+  puts "x is 5"
+end
+if x == 6
+  puts "x is 6"
+end
+if x == 7
+  puts "x is 7"                        
+end
