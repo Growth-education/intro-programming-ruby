d8683bf590ea863632d57bbfcda45799582416ff
Andreas Bourakis <bourakis@gmail.com>
2023-08-07 10:46:56 +0300

    Update word_sorting.rb

--- Chapter-14/word_sorting.rb
+++ Chapter-14/word_sorting.rb
@@ -2,11 +2,11 @@ puts 'Type in as many words as you want. When finished
      press \'Enter\' on an empty line'
 x = 0
 word = 'word1'
-array = []
+words = []
 while word != ''
   word = gets.chomp
-  array[x] = word
+  words[x] = word
   x = x + 1
 end
 puts ''
-puts array.sort
+puts words.sort
